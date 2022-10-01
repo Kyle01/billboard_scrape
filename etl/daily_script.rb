7e553fdd7183350e99b938f1fba0de5bdc4e8d5e
kyle01 <kylemcv@gmail.com>
2022-10-01 12:17:38 -0400

    Create offline json database

--- etl/daily_script.rb
+++ etl/daily_script.rb
@@ -1,7 +1,39 @@
 require_relative 'billboard_scrape'
 require 'dotenv'
+require "json"
 
 Dotenv.load
 
-data = BillboardScrape.new.call
-puts data
+file = File.open "./db.json"
+data_base = JSON.load file
+
+this_weeks_hits = BillboardScrape.new.call
+
+hits_json = []
+this_weeks_hits.each do |hit|
+    hits_json.push({
+        "hit_number": hit.hit_number,
+        "song_title": hit.song_title,
+        "artist": hit.artist,
+        "momentum_symbol": hit.momentum_symbol,
+        "award_info": hit.award_info,
+        "last_week_position": hit.last_week_position,
+        "peak_position": hit.peak_position,
+        "wks_on_chart": hit.wks_on_chart
+    })
+end
+new_data_base_entry = {
+    "date": Time.now.strftime("%m/%d/%Y"),
+    "hits": hits_json
+}
+
+data_base.push(new_data_base_entry)
+
+puts data_base
+
+File.open("./db.json","w") do |f|
+    f.write(data_base.to_json)
+end
+
+
+# puts data
