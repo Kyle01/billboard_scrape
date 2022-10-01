e58450312602726b88bd7d13502bf837b626093c
kyle01 <kylemcv@gmail.com>
2022-10-01 13:25:02 -0400

    Render hits on the frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,19 @@
 <script>
-    export let data;
+    import database from '../../../etl/db.json';
+
+    const thisWeeksHits = database[0]
+
+    console.log(database)
 </script>
   
-<h1 class='text-green-600'>{data.title}</h1>
-<div>{data.content}</div>
+<h1 class='text-green-600'>Billboard hits for the week of {thisWeeksHits.date}</h1>
+{#each thisWeeksHits.hits as hit}
+        <div class='flex border h-8'>
+            <p>{hit.last_week_position}</p>
+            <p>{hit.hit_number}</p>
+            <p>{hit.song_title}</p>
+            <p>{hit.artist}</p>
+            <p>{hit.peak_position}</p>
+            <p>{hit.wks_on_chart}</p>
+        </div>
+{/each}
