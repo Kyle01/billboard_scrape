99e82691f68df037213ad841a8e0f7ef4b12a644
kyle01 <kylemcv@gmail.com>
2022-11-01 22:31:05 -0400

    Work through styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,17 +3,23 @@
 
     const thisWeeksHits = database[0]
 
-    console.log(database)
+    // console.log(database)
 </script>
-  
-<h1 class='text-green-600'>Billboard hits for the week of {thisWeeksHits.date}</h1>
-{#each thisWeeksHits.hits as hit}
-        <div class='flex border h-8'>
-            <p>{hit.last_week_position}</p>
-            <p>{hit.hit_number}</p>
-            <p>{hit.song_title}</p>
-            <p>{hit.artist}</p>
-            <p>{hit.peak_position}</p>
-            <p>{hit.wks_on_chart}</p>
-        </div>
-{/each}
+ 
+<div class='flex my-8'>
+    <h1 class='text-red-600'>Billboard hits for the week of {thisWeeksHits.date}</h1>
+    <div class='divide-y-2 divide-black border-y-2 border-black'>
+        {#each thisWeeksHits.hits as hit}
+            <div class='grid grid-cols-12 items-center'>
+                <p class='text-center'>{hit.last_week_position}</p>
+                <p class='text-center text-white bg-black'>{hit.hit_number}</p>
+                <div class='col-span-8 flex justify-between'>
+                    <strong>{hit.song_title.toUpperCase()}</strong>
+                    <p>{hit.artist}</p>
+                </div>
+                <p class='text-center'>{hit.peak_position}</p>
+                <p class='text-center'>{hit.wks_on_chart}</p>
+            </div>
+        {/each}
+    </div>
+</div>
